f8f6314b3c8b4ef0a2bb4c859cd7b465d4be79eb
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-03-18 15:20:33 +0100

    1.5 update

---
+++ 1.5/Source/VanillaAnimalsExpanded/HarmonyPatches/MapTemperature_SeasonAcceptableFor_Patch.cs
@@ -0,0 +1,45 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+using Verse.AI;
+
+namespace VanillaAnimalsExpanded
+{
+
+    [HarmonyPatch(typeof(MapTemperature))]
+    [HarmonyPatch("SeasonAcceptableFor")]
+    public static class VanillaAnimalsExpanded_MapTemperature_SeasonAcceptableFor_Patch
+    {
+        [HarmonyPostfix]
+        public static void AllowAnimalSpawns(ThingDef animalRace, ref bool __result, Map ___map)
+
+        {
+            RiverNeedingAnimalDef riverAnimals = DefDatabase<RiverNeedingAnimalDef>.GetNamed("AEXP_RiverNeedingAnimals");
+
+
+            if (animalRace != null && riverAnimals.riverNeedingAnimals.Contains(animalRace))
+            {
+                if (___map.TileInfo.Rivers==null)
+                {
+                    __result = false;
+                }
+
+            }
+
+
+
+
+        }
+    }
+
+
+
+
+
+}
---
+++ 1.5/Source/VanillaAnimalsExpanded/VanillaAnimalsExpanded.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using HarmonyLib;
+
+namespace VAEShrubland
+{
+
+    public class VAEShrubland : Mod
+    {
+        public VAEShrubland(ModContentPack content) : base(content)
+        {
+            HarmonyInstance = new Harmony("OskarPotocki.VAEShrubland");
+        }
+
+        public static Harmony HarmonyInstance;
+
+    }
+
+}
